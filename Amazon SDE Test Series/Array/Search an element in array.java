67fcc16569166d335fd5612eeef466a65dcb4c92
Palak <mishra.palak2610@gmail.com>
2020-10-02 13:07:04 +0530

    Create Search an element in array.java

---
+++ Amazon SDE Test Series/Array/Search an element in array.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Array
+{
+	public static void main(String args[]) throws IOException
+	{
+		BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(read.readLine());
+		while(t-- > 0)
+		{
+			int sizeOfArray = Integer.parseInt(read.readLine());
+			int arr[] = new int[sizeOfArray];
+			
+			String st[] = read.readLine().trim().split("\\s+");
+			for(int i =0; i < sizeOfArray; i++)
+			{
+				arr[i] = Integer.parseInt(st[i]);
+			}
+			int x = Integer.parseInt(read.readLine());
+			
+			Searching obj = new Searching();
+			System.out.println(obj.search(arr, sizeOfArray, x));
+		}
+	}
+}
+
+class Searching{
+        
+    static int search(int arr[], int N, int X)
+    {
+    
+        // Your code here
+        for(int i=0;i<N;i++)
+        {
+            if (arr[i]==X)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+}
